8fb201b41b0746a74b545fadfac905dc2195f574
mgkkawa <k.kawate@mg-k.co.jp>
2023-03-14 13:40:52 +0900

    本番環境

--- plugin/js/mobile.js
+++ plugin/js/mobile.js
@@ -1,12 +1,1 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!******************************!*\
-  !*** ./src/mobile/mobile.ts ***!
-  \******************************/
-// You can use the ESModules syntax and @kintone/rest-api-client without additional settings.
-// import { KintoneRestAPIClient } from "@kintone/rest-api-client";
-
-kintone.events.on('mobile.app.record.index.show', function () {});
-/******/ })()
-;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibW9iaWxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUE7QUFDQTs7QUFFQSIsInNvdXJjZXMiOlsid2VicGFjazovL3Bvc251bWJlcjIvLi9zcmMvbW9iaWxlL21vYmlsZS50cyJdLCJzb3VyY2VzQ29udGVudCI6WyIvLyBZb3UgY2FuIHVzZSB0aGUgRVNNb2R1bGVzIHN5bnRheCBhbmQgQGtpbnRvbmUvcmVzdC1hcGktY2xpZW50IHdpdGhvdXQgYWRkaXRpb25hbCBzZXR0aW5ncy5cbi8vIGltcG9ydCB7IEtpbnRvbmVSZXN0QVBJQ2xpZW50IH0gZnJvbSBcIkBraW50b25lL3Jlc3QtYXBpLWNsaWVudFwiO1xuXG5raW50b25lLmV2ZW50cy5vbignbW9iaWxlLmFwcC5yZWNvcmQuaW5kZXguc2hvdycsICgpID0+IHt9KVxuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
+kintone.events.on("mobile.app.record.index.show",(function(){}));
